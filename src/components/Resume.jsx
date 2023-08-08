e7d21f0611e21e5d192487066dba834efff5b11c
etechpro <technov8ive@gmail.com>
2023-08-08 02:19:17 +0100

    updated

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -43,7 +43,7 @@ function Resume() {
           <li>Crypto for Beginners</li>
         </ul>
         <span>
-          <a href="https://cdn.discordapp.com/attachments/1127275147087315034/1137501529427947560/EmmanuelEgboCV.pdf">
+          <a href="https://cdn.discordapp.com/attachments/1127275147087315034/1138279775702614046/EmmanuelEgboCV2.pdf">
           <button className="flex items-center justify-center font-Montserrat text-lg text-black md:text-2xl py-4 font-semibold bg-[#02ec88] 
               hover:bg-[#5ada86] hover:-translate-y-2 transform transition sm:w-[220px] md:w-[240px] md:h-[50px] sm:h-[30px]  rounded-2xl"
             >Download CV</button>
